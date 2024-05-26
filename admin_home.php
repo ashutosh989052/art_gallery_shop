72b07614f3fead954eb6562a769e889cfe786317
ashutosh989052 <ashu989052@gmail.com>
2024-05-26 14:46:24 +0530

    modified:   admin_home.php 	modified:   admin_login.php 	modified:   index.php 	new file:   style2/admin_home_style.css

--- admin_home.php
+++ admin_home.php
@@ -6,7 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Admin Home</title>
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css">
-    <link rel="stylesheet" href="style/admin_style.css">
+    <link rel="stylesheet" href="style2/admin_home_style.css">
 </head>
 
 <body>
